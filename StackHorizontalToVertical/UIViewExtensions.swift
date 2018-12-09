ad6711735659aebe3538dc610fce3bf4925e5516
SandeepSpider811 <sandeep.kumar811@gmail.com>
2018-12-09 20:16:34 +0530

    first commit

---
+++ StackHorizontalToVertical/UIViewExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtensions.swift
+//  StackHorizontalToVertical
+//
+//  Created by Sandeep Kumar on 09/12/18.
+//  Copyright © 2018 Sandeep Kumar. All rights reserved.
+//
+
+import Foundation
