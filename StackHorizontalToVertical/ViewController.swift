d19eef8f5fb50b2b03ae17336088097aa48d0dba
SandeepSpider811 <sandeep.kumar811@gmail.com>
2018-12-09 16:29:18 +0530

    Initial Commit

---
+++ StackHorizontalToVertical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackHorizontalToVertical
+//
+//  Created by Sandeep Kumar on 09/12/18.
+//  Copyright © 2018 Sandeep Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
